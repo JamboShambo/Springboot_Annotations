69c0b039478f8c9d129d3c9e39885cb91f4424f2
JamboShambo <jamiedonohoe2805@gmail.com>
2023-06-12 22:58:04 +0100

    Initial commit

---
+++ src/main/java/com/learning/Springboot_Annotations/SpringbootAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.Springboot_Annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAnnotationsApplication.class, args);
+	}
+
+}
